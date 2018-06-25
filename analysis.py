f4c0679d2d4f8224087ca32446a0bcda2380c776
Corbin Charpentier <corbin.charpentier@gmail.com>
2018-06-24 17:17:42 -0700

    Setup env

---
+++ analysis.py
@@ -0,0 +1,30 @@
+#%%
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+import numpy as np
+import pandas as pd
+
+print("Finished importing")
+
+#%% Read training data
+gender_submission = pd.read_csv("gender_submission.csv")
+print("GENDER_SUBMISSION: " + str(gender_submission.head()))
+print(gender_submission.describe())
+train = pd.read_csv("train.csv")
+print("TRAIN: " + str(train.head()))
+print(train.describe())
+test = pd.read_csv("test.csv")
+print("TEST: " + str(test.head()))
+print(test.describe())
+
+print("End read output")
+
+#%% Clean
+
+#%% Explore
+
+#%% Train
+
+#%% Test
+
+
